e1908c62874f5dd595a49047b406f233ff002399
woodstove-one <vogjeremy@gmail.com>
2026-08-01 13:40:33 -0700

    Update service-worker.js
    duck ai url to absolute

--- audio/service-worker.js
+++ audio/service-worker.js
@@ -1,35 +1,32 @@
 const CACHE_NAME = 'audioflow-cache-v1';
 const ASSETS_TO_CACHE = [
-    './',
-    './audiov15.html',
-    './manifest.json',
-    './icon.svg'
+  'https://woodstove-one.github.io/randomstuff/audio/',
+  'https://woodstove-one.github.io/randomstuff/audio/audiov15.html',
+  'https://woodstove-one.github.io/randomstuff/audio/manifest.json',
+  'https://woodstove-one.github.io/randomstuff/audio/icon.svg'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS_TO_CACHE))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(cached => {
-            if (cached) return cached;
-            return fetch(event.request).then(response => {
-                return response;
-            }).catch(() => {
-                return caches.match('./audiov15.html');
-            });
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() =>
+        caches.match('https://woodstove-one.github.io/randomstuff/audio/audiov15.html')
+      );
+    })
+  );
 });
